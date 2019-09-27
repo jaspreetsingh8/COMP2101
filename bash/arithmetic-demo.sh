39e371875124e99f7b031db187c7e5c8465e129b
jaspreetsingh8 <jaspreetsingh8>
2019-09-27 09:10:59 -0400

    getpics

--- bash/arithmetic-demo.sh
+++ bash/arithmetic-demo.sh
@@ -8,23 +8,21 @@
 #   - the output should look something like:
 #   - first divided by second gives X with a remainder of Y
 # Improve this script by calculating and displaying the first number raised to the power of the second number
-read -p "Input1? " firstnum
-read -p "Input2? " secondnum
-#firstnum=5
-#secondnum=2
-sum=$((firstnum + secondnum))
-sub=$((firstnum - secondnum))
-dividend=$((firstnum / secondnum))
-multiply=$((firstnum * secondnum))
-remainder=$((firstnum % secondnum))
-power=$((firstnum**secondnum))
-fpdividend=$(awk "BEGIN{printf \"%.2f\", $firstnum/$secondnum}")
+read -p "Input1? " firstnumber
+read -p "Input2? " secondnumber
+sum=$((firstnumber + secondnumber))
+sub=$((firstnumber - secondnumber))
+dividend=$((firstnumber / secondnumber))
+multiply=$((firstnumber * secondnumber))
+remainder=$((firstnumber % secondnumber))
+power=$((firstnumber**secondnumber))
+fpdividend=$(awk "BEGIN{printf \"%.2f\", $firstnumber/$secondnumber}")
 
 cat <<EOF
-$firstnum plus $secondnum is $sum
-$firstnum divided by $secondnum gives $dividend with reminder of $remainder
+$firstnumber plus $secondnumber is $sum
+$firstnumber divided by $secondnumber gives $dividend with reminder of $remainder
   - More precisely, dividend is $fpdividend
-$firstnum multiply by $secondnum is $multiply
-$firstnum subtracting by $secondnum is $sub
-Displaying the $firstnum  raised to the power of the $secondnum  is $power
+$firstnumber multiply by $secondnumber is $multiply
+$firstnumber subtracting by $secondnumber is $sub
+Displaying the $firstnumber raised to the power of the $secondnumber is $power
 EOF
